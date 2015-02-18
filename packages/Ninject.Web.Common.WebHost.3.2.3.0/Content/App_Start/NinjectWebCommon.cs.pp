11e509d8e0f9178ae620a379ac33ffdb168753b2
Matthew Dai <matthew.dai@tokiomarinetech.com>
2015-02-18 12:46:52 -0500

    Removed project unittests which is not initlizaed well

--- packages/Ninject.Web.Common.WebHost.3.2.3.0/Content/App_Start/NinjectWebCommon.cs.pp
+++ packages/Ninject.Web.Common.WebHost.3.2.3.0/Content/App_Start/NinjectWebCommon.cs.pp
@@ -1,7 +1,7 @@
-[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(SportsStore.UnitTests.App_Start.NinjectWebCommon), "Start")]
-[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(SportsStore.UnitTests.App_Start.NinjectWebCommon), "Stop")]
+﻿[assembly: WebActivatorEx.PreApplicationStartMethod(typeof($rootnamespace$.App_Start.NinjectWebCommon), "Start")]
+[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof($rootnamespace$.App_Start.NinjectWebCommon), "Stop")]
 
-namespace SportsStore.UnitTests.App_Start
+namespace $rootnamespace$.App_Start
 {
     using System;
     using System.Web;
