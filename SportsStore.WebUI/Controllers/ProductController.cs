ba35e45d0286559e81a1f802329aa98d5606f32a
Matthew Dai <matthew.dai@tokiomarinetech.com>
2015-02-18 22:38:44 -0500

    created Product controller and list action to show product list, and changed the default router to product/list

---
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using SportsStore.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductsRepository repository;
+
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="productRepository"></param>
+        public ProductController(IProductsRepository productRepository)
+        {
+            repository = productRepository;
+        }
+
+
+
+        /// <summary>
+        /// Action to list all products
+        /// </summary>
+        /// <returns></returns>
+        public ViewResult List()
+        {
+            return View(repository.Products);
+        }
+
+ 
+	}
+}
