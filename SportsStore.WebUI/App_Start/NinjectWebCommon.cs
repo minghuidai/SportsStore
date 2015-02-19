62707c27dcd7efb9a000670e6ee8d74337710bcd
Matthew Dai <matthew.dai@tokiomarinetech.com>
2015-02-18 20:54:39 -0500

    Create teh Ninject dependency resolver in the MVC project

--- SportsStore.WebUI/App_Start/NinjectWebCommon.cs
+++ SportsStore.WebUI/App_Start/NinjectWebCommon.cs
@@ -61,6 +61,7 @@ namespace SportsStore.WebUI.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
+            System.Web.Mvc.DependencyResolver.SetResolver(new Infrastructure.NinjectDependencyResolver(kernel));
         }        
     }
 }
---
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,53 @@
+﻿using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+
+        /// <summary>
+        /// constructor
+        /// </summary>
+        /// <param name="kernelParam"></param>
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+
+
+        private void AddBindings()
+        {
+            // put bindings here
+        }
+
+
+        /// <summary>
+        /// Get an instance of server type
+        /// </summary>
+        /// <param name="serviceType"></param>
+        /// <returns></returns>
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+
+        /// <summary>
+        /// Get all instances for a service type
+        /// </summary>
+        /// <param name="serviceType"></param>
+        /// <returns></returns>
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+    }
+}
