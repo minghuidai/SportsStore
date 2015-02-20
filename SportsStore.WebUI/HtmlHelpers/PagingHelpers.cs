b5cfdb92b66d16f0e917fca4a6d735bc8e041c3c
minghuidai <minghuidai@hotmail.com>
2015-02-20 16:16:21 -0500

    Created a PageLinks entension method for HtmlHelper to generate page links on the view.

---
+++ SportsStore.WebUI/HtmlHelpers/PagingHelpers.cs
@@ -0,0 +1,43 @@
+﻿using SportsStore.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.HtmlHelpers
+{
+    public static class PagingHelpers
+    {
+
+        /// <summary>
+        /// Create an extrnsion method for HtmlHelper to generate page links
+        /// </summary>
+        /// <param name="html"></param>
+        /// <param name="pagingInfo"></param>
+        /// <param name="pageUrl"></param>
+        /// <returns></returns>
+        public static MvcHtmlString PageLinks(this HtmlHelper html, PagingInfo pagingInfo, Func<int, string> pageUrl)
+        {
+            var sb = new StringBuilder();
+
+            for (int i = 1; i <= pagingInfo.TotalPages; i++)
+            {
+                var tag = new TagBuilder("a");
+                tag.MergeAttribute("href", pageUrl(i));
+                tag.InnerHtml = i.ToString();
+
+                if (i == pagingInfo.CurrentPage)
+                {
+                    tag.AddCssClass("selected");
+                    tag.AddCssClass("btn-primary");
+                }
+                tag.AddCssClass("btn btn-default");
+                sb.Append(tag.ToString());
+            }
+
+            return MvcHtmlString.Create(sb.ToString());
+        }
+    }
+}
