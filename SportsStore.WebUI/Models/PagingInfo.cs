e2ca42d75fc9a585398acdb7c9bc474472d3ee8a
minghuidai <minghuidai@hotmail.com>
2015-02-20 15:37:43 -0500

    Create PagingInfo viewmodel

---
+++ SportsStore.WebUI/Models/PagingInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SportsStore.WebUI.Models
+{
+    public class PagingInfo
+    {
+        public int TotalItems { get; set; }
+        public int ItemsPerPage { get; set; }
+        public int CurrentPage { get; set; }
+
+
+        /// <summary>
+        /// Get total number of pages
+        /// </summary>
+        public int TotalPages
+        {
+            get { return (int)Math.Ceiling((decimal)TotalItems / ItemsPerPage); }
+        }
+    }
+}
